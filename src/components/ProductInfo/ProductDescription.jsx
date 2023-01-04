1cb2c35c36fe40a4b1849afbe99aabb6ce548329
Andrés <andrescn1007@gmail.com>
2023-01-04 14:45:29 -0400

    ProductInfo

--- src/components/ProductInfo/ProductDescription.jsx
+++ src/components/ProductInfo/ProductDescription.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getUserCart } from '../../store/slices/cart.slice'
 import getConfig from '../../utils/getConfig'
+import './styles/productDescription.css'
 
 const ProductDescription = ({ product }) => {
 
@@ -31,23 +32,29 @@ const ProductDescription = ({ product }) => {
     }
 
     return (
-        <article>
-            <h2>{product?.title}</h2>
-            <p>{product?.description}</p>
-            <section>
-                <span>Price</span>
-                <h3>{product?.price}</h3>
-            </section>
-            <section>
-                <h3>Quantity</h3>
-                <div>
-                    <button onClick={handleMinus}>-</button>
-                    <div>{counter}</div>
-                    <button onClick={handlePlus}>+</button>
+        <section className='product-description-container'>
+            <h2 className='product__title'>{product?.title}</h2>
+            <div className='product-data'>
+                <div className='product-options'>
+                    <div className='product-price-and-quantity'>
+                        <div className='product-price'>
+                            <span className='product-price__label'>Price</span>
+                            <span className='product-price__amount'>{product?.price}</span>
+                        </div>
+                        <div className='product-quantity'>
+                            <div className='product-quantity__label'>Quantity</div>
+                            <div className='product-quantity__counter'>
+                                <button className='product-quantity__counter__minus' onClick={handleMinus}>-</button>
+                                <div className='product-quantity__counter__number'>{counter}</div>
+                                <button className='product-quantity__counter__plus' onClick={handlePlus}>+</button>
+                            </div>
+                        </div>
+                    </div>
+                    <button className='add-to-cart-btn' onClick={handleAddToCart}>Add to cart <i className="fa-solid fa-cart-plus"></i></button>
                 </div>
-            </section>
-            <button onClick={handleAddToCart}>Add to cart <i className="fa-solid fa-cart-plus"></i></button>
-        </article>
+                <p className='product-description'>{product?.description}</p>
+            </div>
+        </section>
     )
 }
 
--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import CardProduct from '../components/home/CardProduct'
 import ProductDescription from '../components/ProductInfo/ProductDescription'
+import '../components/ProductInfo/styles/productInfo.css'
 
 const ProductInfo = () => {
 
     const { id } = useParams()
 
     const [product, setProduct] = useState()
+    const [slider, setSlider] = useState(0)
+    const [selectedImg, setSelectedImg] = useState(1)
     const [similarProducts, setSimilarProducts] = useState()
 
     const products = useSelector(state => state.products)
@@ -28,27 +31,78 @@ const ProductInfo = () => {
         }
     }, [products, product])
 
+    console.log(product)
+
+    const handleMinus = () => {
+        const handleTransform = 100 / product?.productImgs.length
+        if (slider - handleTransform >= 0) {
+            setSlider(slider - handleTransform)
+            setSelectedImg(selectedImg - 1)
+        }
+    }
+
+    const handlePlus = () => {
+        const handleTransform = 100 / product?.productImgs.length
+        if (slider + handleTransform < 100) {
+            setSlider(slider + handleTransform)
+            setSelectedImg(selectedImg + 1)
+        }
+    }
+
     return (
-        <div>
-            <ProductDescription
-                product={product}
-            />
-            <section>
-                <h2>Discover similar items</h2>
-                <div className='similar-products-container'>
+        <div className='page-product-description-container'>
+            <div className='site'>
+                <Link className='site__link' to={'/'}>Home</Link>
+                <div className='circle'></div>
+                <b>{product?.title}</b>
+            </div>
+            <div className='product-container'>
+                <div className='slider-container'>
+                    <div className='img-container'>
+                        <div className='slider__left-btn'><button onClick={handleMinus}>&lt;</button></div>
+                        <div className='slider__right-btn'><button onClick={handlePlus}>&gt;</button></div>
+                        <ul className='slider__img-list' style={{ width: `${product?.productImgs.length * 100}%`, transform: `translate(-${slider}%)` }}>
+                            {
+                                product?.productImgs.map(e => (
+                                    <li key={e} className='slider__img-item'>
+                                        <img className='slider__img--img' src={e} alt="" />
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    <ul className='slider__img-preview'>
+                        {
+                            product?.productImgs.map((e, i) => (
+                                <li key={e} className={`slider__img-item-preview ${selectedImg == 1 + i && 'selected-img'}`}>
+                                    <img className='slider__img--img-preview' src={e} alt="" />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <ProductDescription
+                    product={product}
+                />
+            </div>
+            <section className='suggestions-container'>
+                <h2 className='suggestions__h2'>Discover similar items</h2>
+                <ul className='similar-products-container'>
                     {
                         similarProducts?.map(e => {
                             if (e.title !== product.title) {
                                 return (
-                                    <CardProduct
-                                        key={e.id}
-                                        product={e}
-                                    />
+                                    <li className='similar-products'>
+                                        <CardProduct
+                                            key={e.id}
+                                            product={e}
+                                        />
+                                    </li>
                                 )
                             }
                         })
                     }
-                </div>
+                </ul>
             </section>
         </div>
     )
